fad8d8db03f7fd27ab5b6c337d69d8ba9a408522
Arslan <arslanrazzaq236@gmail.com>
2023-03-02 13:57:37 +0500

    merge mul into calculaton

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function div(a,b) {
     return a/b;
 }
@@ -9,10 +8,8 @@ function sum(a,b) {
     
 }
 console.log(sum(2,3));
-=======
 function mul(a,b) {
     return a*b;
 }
 const multiply=mul(2,3)
 console.log(multiply);
->>>>>>> 6712dbf8caa4ea3ec3038733672d425863c6320e
