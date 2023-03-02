f98c9268d9e43d6d5d69669a96e278970fd3976e
Arslan <arslanrazzaq236@gmail.com>
2023-03-02 13:55:59 +0500

    merge in calculation

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
 function div(a,b) {
     return a/b;
 }
 console.log(div(16,4));
 
-=======
 function sum(a,b) {
     return a+b;
     
 }
 console.log(sum(2,3));
->>>>>>> 878ef67c1d2a3c1ce32079a282dd189a7b5d8475
